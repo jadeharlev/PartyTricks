3f2fb17fefe25de6c79802985dafbf40ae8ca1a5
Joshua Harlev <8244534+joshua-harlev@users.noreply.github.com>
2025-10-01 15:34:49 -0700

    Disable GameState

--- Assets/Scripts/Game/GameState.cs
+++ Assets/Scripts/Game/GameState.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
-    private enum GameStates {
-        MainMenu,
-        Shop,
-        PlayingMinigame,
-        Paused,
-        EndOfGame
-    }
-
-    private GameStates currentState;
-        
-    void Start()
-    {
-        DontDestroyOnLoad(this.gameObject);
-        currentState = GameStates.MainMenu;
-    }
+    // private enum GameStates {
+    //     MainMenu,
+    //     Shop,
+    //     PlayingMinigame,
+    //     Paused,
+    //     EndOfGame
+    // }
+    //
+    // private GameStates currentState;
+    //     
+    // void Start()
+    // {
+    //     DontDestroyOnLoad(this.gameObject);
+    //     currentState = GameStates.MainMenu;
+    // }
 }

5ea0ef250be08fb82fa41519970b495b6a649f82
Joshua Harlev <8244534+joshua-harlev@users.noreply.github.com>
2025-10-01 17:22:31 -0700

    Refactor Shop

--- Assets/Scripts/Shop/Shop.cs
+++ Assets/Scripts/Shop/Shop.cs
@@ -8,103 +8,49 @@ public class Shop : MonoBehaviour {
     [SerializeField] private PlayerCornerDisplay[] PlayerCornerDisplays;
     [SerializeField] public ShopItemsDisplay ShopItemDisplay;
     [SerializeField] private ShopTimer ShopTimer;
+    private ShopPlayerManager playerManager;
     private ShopNavigationService shopNavigationService;
     private ShopPurchaseService shopPurchaseService;
     public int GridRows = 2;
     public int GridColumns = 2;
     public int ShopDurationInSeconds = 10;
-    private List<ShopSlotSelector> activeShopPlayerSelectors = new List<ShopSlotSelector>();
 
     private void Start() {
+        InitializeComponents();
+        StartShop();
+    }
+
+    private void InitializeComponents() {
         ShopItemDisplay.SetShopItemUIElements(ShopItemUIElements);
         ShopItemDisplay.SetUpItems();
         shopPurchaseService = new ShopPurchaseService();
         shopNavigationService = new ShopNavigationService(GridRows, GridColumns);
-        InitializePlayerSelectors();
-        InitializePlayerCornerDisplays();
-        StartShopLogic();
-        ShopTimer.OnTimerEnd += ResolvePurchases;
+        playerManager = new ShopPlayerManager(shopNavigationService, ShopItemUIElements, PlayerCornerDisplays);
+        ShopTimer.OnTimerEnd += OnShopTimerEnd;
     }
 
-    private void InitializePlayerCornerDisplays() {
-        for (int i = 0; i < PlayerCornerDisplays.Length; i++) {
-            PlayerCornerDisplays[i].Initialize(GameSessionManager.Instance.PlayerSlots[i].Profile);
-        }
+    private void StartShop() {
+        playerManager.InitializePlayers();
+        ShopTimer.StartTimer(ShopDurationInSeconds);
+    }
+    
+    private void OnShopTimerEnd() {
+        shopPurchaseService.ResolvePurchases(playerManager.GetSelectors(), ShopItemUIElements);
     }
 
     public void Reset() {
         ShopTimer.Reset();
         ShopTimer.StartTimer(ShopDurationInSeconds);
-        foreach (ShopSlotSelector selector in activeShopPlayerSelectors) {
-            selector.CanAct = true;
-        }
+        playerManager.EnableAllSelectors();
     }
 
     public void UnlockAISelectors() {
-        foreach (ShopSlotSelector selector in activeShopPlayerSelectors) {
-            if (selector.Navigator is AIShopInputHandler) {
-                selector.Unlock();
-            }
-        }
+        playerManager.UnlockAISelectors();
     }
+    
 
     private void OnDestroy() {
-        ShopTimer.OnTimerEnd -= ResolvePurchases;
-        foreach (var playerSelector in activeShopPlayerSelectors) {
-            playerSelector.OnSelectionChanged -= HandleSelectionChanged;
-            playerSelector.OnLockChanged -= HandleLockChanged;
-        }
-    }
-
-    private void InitializePlayerSelectors() {
-        if (GameSessionManager.Instance == null) {
-            Debug.LogError("Error: GameSessionManager.Instance is null");
-            return;
-        }
-
-        activeShopPlayerSelectors.Clear();
-        for (int i = 0; i < GameSessionManager.Instance.PlayerSlots.Length; i++) {
-            PlayerSlot playerSlot = GameSessionManager.Instance.PlayerSlots[i];
-            if (playerSlot.Navigator == null) continue;
-            CreatePlayerShopSlotSelector(i, playerSlot);
-        }
-    }
-
-    private void CreatePlayerShopSlotSelector(int playerIndex, PlayerSlot playerSlot) {
-        var shopSlotSelector = CreateShopSlotSelectorGameObject(playerIndex, playerSlot);
-        activeShopPlayerSelectors.Add(shopSlotSelector);
-    }
-
-    private ShopSlotSelector CreateShopSlotSelectorGameObject(int playerIndex, PlayerSlot playerSlot) {
-        GameObject selectorGameObject = new GameObject("Player_"+playerIndex+"_ShopSlotSelector");
-        selectorGameObject.transform.SetParent(transform);
-        ShopSlotSelector shopSlotSelector = selectorGameObject.AddComponent<ShopSlotSelector>();
-        shopSlotSelector.Initialize(playerIndex, playerSlot.Navigator, shopNavigationService, playerSlot.Profile);
-        return shopSlotSelector;
-    }
-
-    public void StartShopLogic() {
-        foreach (var playerSelector in activeShopPlayerSelectors) {
-            playerSelector.OnSelectionChanged += HandleSelectionChanged;
-            playerSelector.OnLockChanged += HandleLockChanged;
-            HandleSelectionChanged(playerSelector, playerSelector.CurrentShopItemIndex);
-        }
-
-        ShopTimer.StartTimer(ShopDurationInSeconds);
-    }
-
-    private void HandleSelectionChanged(ShopSlotSelector slotSelector, int newIndex) {
-        for (int i = 0; i < ShopItemUIElements.Length; i++) {
-            bool isCurrentlySelected = (i == newIndex);
-            ShopItemUIElements[i].OnPointedTo(slotSelector.PlayerIndex, isCurrentlySelected, slotSelector.IsLocked);
-        }
-    }
-
-    private void HandleLockChanged(ShopSlotSelector slotSelector, bool locked) {
-        ShopItemUIElements[slotSelector.CurrentShopItemIndex].OnPointedTo(slotSelector.PlayerIndex, true, locked);
-    }
-
-    private void ResolvePurchases() {
-        shopPurchaseService.ResolvePurchases(activeShopPlayerSelectors, ShopItemUIElements);
+        ShopTimer.OnTimerEnd -= OnShopTimerEnd;
+        playerManager?.Cleanup();
     }
 }
--- Assets/Scripts/Shop/ShopPurchaseService.cs
+++ Assets/Scripts/Shop/ShopPurchaseService.cs
@@ -5,22 +5,24 @@ public class ShopPurchaseService {
     public void ResolvePurchases(List<ShopSlotSelector> shopSlotSelectors, ShopItemUI[] shopItems) {
         Debug.Log("Shop closed! Resolving purchases.");
         foreach (var playerSelector in shopSlotSelectors) {
-            int index = playerSelector.CurrentShopItemIndex;
-            playerSelector.Lock();
-            playerSelector.CanAct = false;
-            PlayerSlot slot = GameSessionManager.Instance.PlayerSlots[playerSelector.PlayerIndex];
-            PlayerProfile profile = slot.Profile;
-            ShopItemUI item = shopItems[index];
-            bool purchaseSuccess = profile.Wallet.Buy(item.GetItemCost());
-            if (purchaseSuccess) {
-                profile.Inventory.AddItem(item.GetItem().ToDefinition());
-                Debug.Log("Shop.cs: Player " + playerSelector.PlayerIndex + " buys " + shopItems[index].ToString());
-            }
-            else {
-                Debug.Log("Shop.cs: Player " + playerSelector.PlayerIndex + " could not afford " + shopItems[index].ToString());
-            }
+            ProcessPurchase(shopItems, playerSelector);
         }
     }
 
-    
+    private static void ProcessPurchase(ShopItemUI[] shopItems, ShopSlotSelector playerSelector) {
+        int index = playerSelector.CurrentShopItemIndex;
+        playerSelector.Lock();
+        playerSelector.CanAct = false;
+        PlayerSlot slot = GameSessionManager.Instance.PlayerSlots[playerSelector.PlayerIndex];
+        PlayerProfile profile = slot.Profile;
+        ShopItemUI item = shopItems[index];
+        bool purchaseSuccess = profile.Wallet.Buy(item.GetItemCost());
+        if (purchaseSuccess) {
+            profile.Inventory.AddItem(item.GetItem().ToDefinition());
+            Debug.Log("Shop.cs: Player " + playerSelector.PlayerIndex + " buys " + shopItems[index].ToString());
+        }
+        else {
+            Debug.Log("Shop.cs: Player " + playerSelector.PlayerIndex + " could not afford " + shopItems[index].ToString());
+        }
+    }
 }

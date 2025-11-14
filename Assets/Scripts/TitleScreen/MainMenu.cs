a466f77e4b8961f4768e666239f9004cd421cc89
Jade Harlev <8244534+jadeharlev@users.noreply.github.com>
2025-11-14 13:56:22 -0800

    Remove unneccesary import

--- Assets/Scripts/TitleScreen/MainMenu.cs
+++ Assets/Scripts/TitleScreen/MainMenu.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour {
